c396d693365d6591567eccfde24f305954ff0efc
valenciomuskiet <valenciomuskiet.ict@gmail.com>
2022-06-19 17:09:50 +0200

    commit 19
    test 1

---
+++ ProjectDepotTwo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjectDepotTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
