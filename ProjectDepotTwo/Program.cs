b0c2a2875f2b840a9092d7ccf40f220c97426169
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-28 07:57:41 +0200

    Handmatig merge conflict solving

--- ProjectDepotTwo/Program.cs
+++ ProjectDepotTwo/Program.cs
@@ -1,59 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 
+
 namespace ProjectDepotTwo
 {
-    class Program // class program
+    public class Program
     {
         static void Main()
         {
-<<<<<<< Updated upstream
-
+            ApplicatieComponentBezoeker bezoeker = new ApplicatieComponentBezoeker();  // declaring bezoekersdeel
+            ApplicatieComponentGids gids = new ApplicatieComponentGids();              // declaring gidsdeel
             int component = 0;
             while (component != -1)
             {
-                ApplicatieComponentBezoeker bezoeker = new ApplicatieComponentBezoeker();
-                ApplicatieComponentGids gids = new ApplicatieComponentGids();
-
                 switch (component)                                                 
-=======
-            ApplicatieComponentBezoeker bezoeker = new ApplicatieComponentBezoeker();  
-            ApplicatieComponentGids gids = new ApplicatieComponentGids();              
-            int component = 0;
-            while (component != -1)
-            {
-                switch (component)
->>>>>>> Stashed changes
                 {
                     case 0:
-                        component = StartSchermComponent();
+                        component = StartSchermComponent();             // startscherm 
                         break;
                     case 1:
-                        component = bezoeker.BezoekersComponent();
+                        component = bezoeker.BezoekersComponent();      // Bezoekers deel
                         break;
                     case 2:
-                        component = gids.GidsComponent();
+                        component = gids.GidsComponent();               // gids deel
                         break;
                 }
             }
         }
-<<<<<<< Updated upstream
 
-
-        private static int StartSchermComponent()                                     
-=======
         private static int StartSchermComponent()                       
->>>>>>> Stashed changes
         {
             {
                 while (true)
                 {
                     Console.Clear();
-                    Console.Write("Depot Boijmans Van Beuningen\n\n[1] Bezoeker\n[2] Gids\n\n\n\nSelecteer uw optie :");
-
+                    Console.Write("Welkom bij het Depot Boijmans Van Beuningen\n\n[1] Bezoeker\n[2] Gids\n\n\n\nSelecteer uw optie :");
                     string invoergebruiker = Console.ReadLine();
                     switch (invoergebruiker)
                     {
@@ -62,12 +46,12 @@ namespace ProjectDepotTwo
                         case "2":
                             return 2;
                         default:
-                            Console.WriteLine("Invoer onjuist. Toets [Enter] om het opnieuw te proberen");
+                            Console.WriteLine("Invoer onjuist, selecteer een van bovenstaande opties a.u.b.");
                             Console.ReadLine();
                             break;
                     }
                 }
             }
         }
-    }
+	}
 }
